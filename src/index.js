dbb700c8b5180b96c8bfc7d7a828587476fc871f
liranfar <l.farage@gmail.com>
2018-03-18 13:51:00 +0200

    combine-reducers

--- src/index.js
+++ src/index.js
@@ -4,38 +4,27 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import { createStore } from 'redux'
+import { combineReducers, createStore } from 'redux'
 
 //reducer is listening to every action that is sent
-function reducer(state, action) {
-
-    if (action.type === 'changeState'){
-        // the payload becomes the new state
-        return action.payload.newState;
-    }
-
-    return 'State'
+function productsReducer(state = [], action) {
+    return state;
 }
+
+function userReducer(state = '', action) {
+    return state
+}
+
+const allReducers = combineReducers({
+    products: productsReducer,
+    user: userReducer
+});
 
 // creating the store with the reducer injected
-const store = createStore(reducer);
+const store = createStore(allReducers);
 
 console.log(store.getState());
 
-
-//defining an action
-const action = {
-    type: 'changeState',
-    payload: {
-        newState: 'New state'
-    }
-}
-
-
-// invoking an action
-store.dispatch(action);
-
-console.log(store.getState());
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
